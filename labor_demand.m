12b0d1d425b2afee96a62b09d4a5376113a5aa1a
aax010 <m.alexandrakis@qmul.ac.uk>
2024-01-17 20:55:42 +0000

    Pre-calculate common expressions - Need renaming

--- labor_demand.m
+++ labor_demand.m
@@ -8,11 +8,14 @@ function [LSH, LD_HH, LD_LH, LD_IH, LSL, LD_HL, LD_LL, LD_IL] = labor_demand(wH,
     
     G = (b+ (wI/(wL*b))^((gamma)/(1-gamma)) )^((rho-gamma)/gamma);
     J = ( (a*G^(rho/(rho - gamma))) + (wI/(G*a*b*wH))^(rho/(1-rho))  )^((alpha-rho)/(rho));
-    
+
+    commonExp1 = (wI/(wL*b))^(1/(1-gamma));
+    commonExp2 = ( (wI/(G*a*b*wH)) )^(1/(1-rho));
+
     % High skill
     IzH =  ((p.avecH.* (kappa*alpha * J * G * a * b))./(wI)).^(1/(1-alpha));
-    LzH =  IzH .*( (wI/(wL*b))^(1/(1-gamma)) );
-    HzH =  IzH .*( (wI/(G*a*b*wH)) )^(1/(1-rho));
+    LzH =  IzH .*commonExp1;
+    HzH =  IzH .*commonExp2;
     profitH = p.avecH.*kappa.*(   ( ( ((IzH.^gamma).*b + (LzH.^gamma)).*a).^(rho/gamma) + HzH.^rho ).^(alpha/rho)) - (HzH.*wH + LzH.*wL + IzH.*wI);
     wvecH = wH * ones(length(profitH), 1);
     LSH = sum(wvecH > profitH);
@@ -22,8 +25,8 @@ function [LSH, LD_HH, LD_LH, LD_IH, LSL, LD_HL, LD_LL, LD_IL] = labor_demand(wH,
 
     % Low skill
     IzL =  ((p.avecL.* (alpha * J * G * a * b))./(wI)).^(1/(1-alpha));
-    LzL =  IzL .*( (wI/(wL*b))^(1/(1-gamma)) );
-    HzL =  IzL .*( (wI/(G*a*b*wH)) )^(1/(1-rho));
+    LzL =  IzL .*commonExp1;
+    HzL =  IzL .*commonExp2;
     profitL = p.avecL.*(   ( ( ((IzL.^gamma).*b + (LzL.^gamma)).*a).^(rho/gamma) + HzL.^rho ).^(alpha/rho)) - (HzL.*wH + LzL.*wL + IzL.*wI);
     wvecL = wL * ones(length(profitL), 1);
     LSL = sum(wvecL > profitL);
