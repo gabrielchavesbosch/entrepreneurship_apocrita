e55b99d84b74b6f6bcef37eea8eada47f27f2318
aax010 <m.alexandrakis@qmul.ac.uk>
2024-01-18 14:21:36 +0000

    Experimental changes: Moved around pre-determined blocks, pre-computed repeated expressions, took advantage of W being diagonal

--- init_val_calibration.m
+++ init_val_calibration.m
@@ -66,9 +66,6 @@ results = ones(n, 9);
 
 tic
 
-parfor (it = 1:n, numWorkers)
-
-p = [];
 
     % Moments to be matched
     p.data_share_entrepreneur_H_b   = 0.113;  % baseline, data
@@ -88,7 +85,27 @@ p = [];
     p.Ib = p.bins*0.0219;                             %  Chosen to make immigrant share equal baseline immigrant share (= 0.0214)
     p.Ia = p.bins*0.16274;                           %  Chosen to make immigrant share equal to post-shock immigrant share (= 0.14)
     p.shareL = 0.464;
+    p.kappa = 1;
 
+    % For now, diagonal matrix
+    variances = [1,0.5,0.1,0.1,1,0.1,1,1,1];
+    W = diag(variances)^-1 ;
+
+    %p.kappa = parameters(5);
+    options = optimset('Display','none');
+    x0 = [1,1,1];
+
+    % Data moments
+    Mhat = [p.data_share_entrepreneur_H_b, p.data_share_entrepreneur_L_b...
+            , p.data_change_entrepreneur_H,p.data_change_entrepreneur_L...
+            ,p.data_rel_wage_H, p.data_rel_wage_L ...
+            ,p.data_change_wage_H, p.data_change_wage_L, p.data_rel_wage_I];
+
+    highAllocation = p.bins * (1 - p.shareL);
+    lowAllocation = p.bins * p.shareL;
+
+
+for it = 1:n
     % Rho and gamma (substitution parameters); a and b (rel. productivity low
     % skill and relative productivity immigrants); lognormal mean H, L and
     % lognormal stde H and L
@@ -100,17 +117,11 @@ p = [];
     p.muL    = parameters(it,6);
     p.sigmaH = parameters(it,7);
     p.sigmaL = parameters(it,8);
-    p.kappa = 1;
 
         % distribution of a:
     % nr grid points
-    p.avecH = sort(lognrnd(p.muH,p.sigmaH,p.bins*(1-p.shareL),1));
-    p.avecL = sort(lognrnd(p.muL,p.sigmaL,p.bins*p.shareL,1));
-
-    %p.kappa = parameters(5);
-    options = optimset('Display','none');
-    x0 = [1,1,1];
-
+    p.avecH = sort(lognrnd(p.muH,p.sigmaH,highAllocation,1));
+    p.avecL = sort(lognrnd(p.muL,p.sigmaL,lowAllocation,1));
 
     % Before
     p.I = p.Ib;
@@ -119,8 +130,8 @@ p = [];
     wLb = xb(2);
     wIb = xb(3);
     [LSHb, LD_HHb, LD_LHb, LD_IHb, LSLb, LD_HLb, LD_LLb, LD_ILb] = labor_demand(wHb, wLb, wIb, p);
-    share_entrepreneur_H_b = (p.bins*(1-p.shareL) -(LSHb))/(p.bins*(1-p.shareL));
-    share_entrepreneur_L_b = (p.bins*(p.shareL) -  (LSLb))/(p.bins*(p.shareL));
+    share_entrepreneur_H_b = (highAllocation -(LSHb))/(highAllocation);
+    share_entrepreneur_L_b = (lowAllocation -  (LSLb))/(lowAllocation);
 
     % After
     p.I = p.Ia;
@@ -129,14 +140,8 @@ p = [];
     wLa = xa(2);
     wIa = xa(3);
     [LSHa, LD_HHa, LD_LHa, LD_IHa, LSLa, LD_HLa, LD_LLa, LD_ILa] = labor_demand(wHa, wLa, wIa, p);
-    share_entrepreneur_H_a = (p.bins*(1-p.shareL) -(LSHa))/(p.bins*(1-p.shareL));
-    share_entrepreneur_L_a = (p.bins*(p.shareL) -  (LSLa))/(p.bins*(p.shareL));
-
-    % Data moments
-    Mhat = [p.data_share_entrepreneur_H_b, p.data_share_entrepreneur_L_b...
-            , p.data_change_entrepreneur_H,p.data_change_entrepreneur_L...
-            ,p.data_rel_wage_H, p.data_rel_wage_L ...
-            ,p.data_change_wage_H, p.data_change_wage_L, p.data_rel_wage_I];
+    share_entrepreneur_H_a = (highAllocation -(LSHa))/(highAllocation);
+    share_entrepreneur_L_a = (lowAllocation -  (LSLa))/(lowAllocation);
 
     change_entrepreneur_H  = share_entrepreneur_H_a - share_entrepreneur_H_b;
     change_entrepreneur_L = share_entrepreneur_L_a - share_entrepreneur_L_b;
@@ -151,11 +156,12 @@ p = [];
     MS = [share_entrepreneur_H_b, share_entrepreneur_L_b, change_entrepreneur_H, change_entrepreneur_L...
         , rel_wage_H,  rel_wage_L, change_wage_H, change_wage_L, rel_wage_I];
 
-    % For now, diagonal matrix
-    variances = [1,0.5,0.1,0.1,1,0.1,1,1,1];
-    W = diag(variances)^-1 ;
+    % Q = (MS-Mhat)*W*(MS-Mhat)';
 
-    Q = (MS-Mhat)*W*(MS-Mhat)';
+    diffMS_Mhat = MS - Mhat; % Compute the difference once
+    scaledDiff = diffMS_Mhat ./ variances; % Element-wise scaling
+    Q = scaledDiff * scaledDiff'; % Equivalent to the original, but more efficient
+
 
     results(it,:) = [parameters(it,:), Q];
 
